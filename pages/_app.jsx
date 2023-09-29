e8cdfe553217a43fcff0ea1ecacb469b547dcaa1
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-09-29 19:58:12 +0200

    add emailJS et react-toastify

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'slick-carousel/slick/slick.css';
-import '../styles/globals.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "../styles/globals.scss";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+			<Component {...pageProps} />;
+		</>
+	);
 }
 
 export default MyApp;
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,14 +1,50 @@
 import { Icon } from "@iconify/react";
 import Head from "next/head";
-import React from "react";
 import Div from "../components/Div";
 import Layout from "../components/Layout";
 import PageHeading from "../components/PageHeading";
 import SectionHeading from "../components/SectionHeading";
 import Spacing from "../components/Spacing";
 import ContactInfoWidget from "../components/Widget/ContactInfoWidget";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Contact() {
+	const form = useRef();
+
+	// Configuration emailJS Yoann = à changer
+	const sendEmail = (e) => {
+		e.preventDefault();
+		emailjs.sendForm("service_hdxsuya", "template_zm2ctt7", form.current, "W4SjcLIIqIH2c8aZX").then(
+			(result) => {
+				console.log(result);
+				toast.success("Message envoyé avec succès !", {
+					position: "bottom-right",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+				document.getElementById("myForm").reset();
+			},
+			(error) => {
+				toast.error("Oups, le message n'a pas été envoyé !", {
+					position: "bottom-right",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				});
+			}
+		);
+	};
+
 	return (
 		<>
 			<Head>
@@ -22,26 +58,35 @@ export default function Contact() {
 				<Div className="container">
 					<Div className="row">
 						<Div className="col-lg-6">
-							<SectionHeading title="Parlez moi de <br/> votre projet." subtitle="Disponibilité et réactivité" />
+							<SectionHeading
+								title="Parlez moi de <br/> votre projet."
+								subtitle="Disponibilité et réactivité"
+							/>
 							<Spacing lg="55" md="30" />
 							<ContactInfoWidget withIcon />
 							<Spacing lg="0" md="50" />
 						</Div>
 						<Div className="col-lg-6">
-							<form action="#" className="row">
+							<form id="myForm" ref={form} onSubmit={sendEmail} className="row">
 								<Div className="col-sm-6">
-									<label className="cs-primary_color">Nom complet*</label>
-									<input type="text" className="cs-form_field" />
+									<label htmlFor="name" className="cs-primary_color">
+										Nom complet*
+									</label>
+									<input type="text" name="name" className="cs-form_field" />
 									<Spacing lg="20" md="20" />
 								</Div>
 								<Div className="col-sm-6">
-									<label className="cs-primary_color">Email*</label>
-									<input type="text" className="cs-form_field" />
+									<label htmlFor="user_email" className="cs-primary_color">
+										Email*
+									</label>
+									<input type="email" name="user_email" className="cs-form_field" />
 									<Spacing lg="20" md="20" />
 								</Div>
 								<Div className="col-sm-6">
-									<label className="cs-primary_color">Type de projet*</label>
-									<input type="text" className="cs-form_field" />
+									<label htmlfor="project" className="cs-primary_color">
+										Type de projet*
+									</label>
+									<input type="text" name="project" className="cs-form_field" />
 									<Spacing lg="20" md="20" />
 								</Div>
 								<Div className="col-sm-6">
@@ -50,12 +95,20 @@ export default function Contact() {
 									<Spacing lg="20" md="20" />
 								</Div>
 								<Div className="col-sm-12">
-									<label className="cs-primary_color">Message*</label>
-									<textarea cols="30" rows="7" className="cs-form_field"></textarea>
+									<label htmlFor="message" className="cs-primary_color">
+										Message*
+									</label>
+									<textarea
+										type="text"
+										name="message"
+										cols="30"
+										rows="7"
+										className="cs-form_field"
+									></textarea>
 									<Spacing lg="25" md="25" />
 								</Div>
 								<Div className="col-sm-12">
-									<button className="cs-btn cs-style1">
+									<button type="submit" className="cs-btn cs-style1">
 										<span>Envoyer le message</span>
 										<Icon icon="bi:arrow-right" />
 									</button>
