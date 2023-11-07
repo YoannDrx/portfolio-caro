e0b076d53d14e566a2e4fc3b76a1646d5b42e257
YoannDrx <yoann.andrieux@gmail.com>
2023-11-07 23:53:50 +0100

    fix merge

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,6 +11,7 @@ import Hero3 from "../components/Hero/Hero3";
 import Portfolio2 from "../components/Portfolio/Portfolio2";
 import Portfolio3 from "../components/Portfolio/Portfolio3";
 import CaseStudy from "../components/CaseStudy";
+import ServiceList from "../components/ServiceList";
 import { useState } from "react";
 import Portfolio from "../components/Portfolio";
 import { Icon } from "@iconify/react";
@@ -64,41 +65,7 @@ export default function Home({ services }) {
       links: "/contact",
     },
   ];
-  // Hero Social Links
-  const heroSocialLinks = [
-    {
-      name: "Parigo",
-      links: "https://www.parigomusic.com/",
-    },
-    {
-      name: "LinkedIn",
-      links: "/",
-    },
-    {
-      name: "Contact",
-      links: "/contact",
-    },
-  ];
 
-  // FunFact Data
-  const funfaceData = [
-    {
-      title: "Global Happy Clients",
-      factNumber: "40K",
-    },
-    {
-      title: "Project Completed",
-      factNumber: "50K",
-    },
-    {
-      title: "Team Members",
-      factNumber: "245",
-    },
-    {
-      title: "Digital products",
-      factNumber: "550",
-    },
-  ];
   // FunFact Data
   const funfaceData = [
     {
@@ -153,106 +120,7 @@ export default function Home({ services }) {
       category: "Consultation",
     },
   ];
-  const portfolioData = [
-    {
-      title: "Gestion des droits d'auteur pour artistes indépendants",
-      subtitle: "Projet 01",
-      btnText: "Voir les détails",
-      btnLink: "/portfolio/gestion-droits-auteur",
-      imageUrl: "/images/portfolio_35.jpeg",
-      category: "Droit d'auteur",
-    },
-    {
-      title: "Licences et distribution de musique en ligne",
-      subtitle: "Projet 02",
-      btnText: "Voir les détails",
-      btnLink: "/portfolio/licences-distribution",
-      imageUrl: "/images/portfolio_36.jpeg",
-      category: "Distribution",
-    },
-    {
-      title: "Conformité et audits de droits d'auteur",
-      subtitle: "Projet 03",
-      btnText: "Voir les détails",
-      btnLink: "/portfolio/conformite-audits",
-      imageUrl: "/images/portfolio_37.jpeg",
-      category: "Conformité",
-    },
-    {
-      title: "Consultation en gestion de catalogue musical",
-      subtitle: "Projet 04",
-      btnText: "Voir les détails",
-      btnLink: "/portfolio/gestion-catalogue",
-      imageUrl: "/images/portfolio_38.jpeg",
-      category: "Consultation",
-    },
-  ];
 
-  const projetData = [
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_11.jpeg",
-      category: "ui_ux_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_12.jpeg",
-      category: "logo_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_13.jpeg",
-      category: "web_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_14.jpeg",
-      category: "mobile_apps",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_15.jpeg",
-      category: "ui_ux_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_16.jpeg",
-      category: "web_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_11.jpeg",
-      category: "ui_ux_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_12.jpeg",
-      category: "logo_design",
-    },
-    {
-      title: "Colorful Art Work",
-      subtitle: "See Details",
-      href: "/portfolio/portfolio-details",
-      src: "/images/portfolio_13.jpeg",
-      category: "web_design",
-    },
-  ];
   const projetData = [
     {
       title: "Colorful Art Work",
@@ -379,54 +247,6 @@ export default function Home({ services }) {
           </Div>
         </Div>
         {/* End Projects Section */}
-
-        {/* Start Portfolio Section */}
-        <Spacing lg="150" md="80" />
-        <SectionHeading title="Mes Expertises" subtitle="Services" variant="cs-style1 text-center mb-5" />
-
-        {portfolioData.map((item, index) =>
-          index % 2 === 0 ? (
-            <Div key={index}>
-              <Portfolio2
-                title={item.title}
-                subtitle={item.subtitle}
-                btnText={item.btnText}
-                btnLink={item.btnLink}
-                imageUrl={item.imageUrl}
-                category={item.category}
-              />
-              <Spacing lg="100" md="70" />
-            </Div>
-          ) : (
-            <Div key={index}>
-              <Portfolio3
-                title={item.title}
-                subtitle={item.subtitle}
-                btnText={item.btnText}
-                btnLink={item.btnLink}
-                imageUrl={item.imageUrl}
-                category={item.category}
-              />
-              <Spacing lg="100" md="70" />
-            </Div>
-          )
-        )}
-        {/* End Portfolio Section */}
-          <Div className="text-center">
-            {projetData.length <= itemShow ? (
-              ""
-            ) : (
-              <>
-                <Spacing lg="65" md="40" />
-                <span className="cs-text_btn" onClick={() => setItemShow(itemShow + 3)}>
-                  <span>Load More</span>
-                  <Icon icon="bi:arrow-right" />
-                </span>
-              </>
-            )}
-          </Div>
-        </Div>
-        {/* End Projects Section */}
         {/* Start Portfolio Section */}
         {/* <Spacing lg="150" md="80" />
 				<SectionHeading title="Mes Expertises" subtitle="Services" variant="cs-style1 text-center mb-5" />
@@ -460,25 +280,6 @@ export default function Home({ services }) {
         {/* End Portfolio Section */}
 
         {/* Start Case Study Section */}
-        <Spacing lg="45" md="10" />
-        <Div className="container">
-          <SectionHeading title="Obtention de Subventions" subtitle="Financement" variant="cs-style1 text-center" />
-          <Spacing lg="90" md="45" />
-        </Div>
-        <CaseStudy
-          title="Étude de cas de <br /> subvention réussies"
-          bgUrl="/images/case_study_2.jpeg"
-          href="/case-study-details"
-          variant="cs-style2"
-        />
-        <CaseStudy title="Conseils <br /> et astuces" bgUrl="/images/case_study_1.jpeg" href="/case-study-details" />
-        <Spacing lg="150" md="80" />
-        {/* End Case Study Section */}
-
-        {/* Start Testimonial Section */}
-        <TestimonialSlider />
-        {/* End Testimonial Section */}
-        {/* Start Case Study Section */}
         {/* <Spacing lg="45" md="10" />
 				<Div className="container">
 					<SectionHeading
@@ -504,11 +305,6 @@ export default function Home({ services }) {
 
         {/* Start MovingText Section */}
         <Spacing lg="125" md="70" />
-        <MovingText text="Créez librement. Vos oeuvres entre de bonnes mains." />
-        <Spacing lg="105" md="70" />
-        {/* End MovingText Section */}
-        {/* Start MovingText Section */}
-        <Spacing lg="125" md="70" />
         <div onClick={() => setShowModal(true)}>
           <MovingText text="Le track de la semaine : Téléraptor -- " />
         </div>
@@ -524,26 +320,6 @@ export default function Home({ services }) {
         </Div>
         {/* End MovingText Section */}
 
-        {/* Start LogoList Section */}
-        <Div className="container">
-          <LogoList />
-        </Div>
-        <Spacing lg="150" md="80" />
-        {/* End LogoList Section */}
-
-        {/* Start CTA Section */}
-        <Div className="container">
-          <Cta
-            title="Unissons nos forces pour <br /> protéger et valoriser votre <i>art</i>"
-            btnText="Discutons ensemble"
-            btnLink="/contact"
-            bgSrc="/images/cta_bg_3.jpeg"
-          />
-        </Div>
-        {/* End CTA Section */}
-      </Layout>
-    </>
-  );
         {/* Début de la section Équipe */}
         {/* Vous pouvez supprimer cette section si elle n'est pas pertinente pour vous */}
         <Spacing lg="145" md="80" />
