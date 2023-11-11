e2f417579c4c3dd217f4be96a229df8e61ab5034
YoannDrx <yoann.andrieux@gmail.com>
2023-11-11 23:28:52 +0100

    Add Spacing component and remove marginTop style in ImageAndTextLeft and ImageAndTextRight components. Update margin-top in ul element.

--- components/ImageAndTextLeft.js
+++ components/ImageAndTextLeft.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Div from "./Div";
+import Spacing from "./Spacing";
 
 const ImageAndTextLeft = ({ title, imagePath, altText, children }) => {
   return (
-    <Div className="container" style={{ marginTop: "90px" }}>
+    <Div className="container">
       <Div className="">
         <h2 className="text-center" style={{ color: "#ff4b17", marginBlock: "50px", marginTop: "50px" }}>
           {title}
@@ -13,6 +14,7 @@ const ImageAndTextLeft = ({ title, imagePath, altText, children }) => {
         </Div>
         <Div className="cs-post cs-style2">{children}</Div>
       </Div>
+      <Spacing lg="30" md="50" />
     </Div>
   );
 };
