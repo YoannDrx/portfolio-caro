2de056192371a9037296da1f7fd50533fa1a7ddd
YoannDrx <yoann.andrieux@gmail.com>
2023-11-17 22:48:42 +0100

    Fix filter Masonry

--- components/DocumentairesGallery.js
+++ components/DocumentairesGallery.js
@@ -7,7 +7,16 @@ import Div from "./Div";
 
 export default function DocumentairesGallery({ labels, documentaires }) {
   const [active, setActive] = useState("all");
-  const [itemShow, setItemShow] = useState(10);
+  const [visibleCount, setVisibleCount] = useState(10); // Nombre d'éléments visibles
+
+  const changeFilterMode = (category) => {
+    setActive(category);
+    setVisibleCount(10); // Réinitialiser le nombre d'éléments visibles lors du changement de catégorie
+  };
+
+  console.log("active >>", active);
+
+  // console.log("documentaires >>", documentaires);
 
   const portfolioData = [
     {
@@ -139,30 +148,23 @@ export default function DocumentairesGallery({ labels, documentaires }) {
   ];
   const categoryMenu = [
     {
-      title: "Wedding",
-      category: "wedding",
+      title: "Little big story",
+      category: "little-big-story",
     },
     {
-      title: "Portrait",
-      category: "portrait",
+      title: "13 prods",
+      category: "13-prods",
     },
     {
-      title: "Fashion",
-      category: "fashion",
+      title: "Via découvertes",
+      category: "via-decouvertes-films",
     },
     {
-      title: "Commercial",
-      category: "commercial",
-    },
-    {
-      title: "Landscape",
-      category: "landscape",
-    },
-    {
-      title: "Short film",
-      category: "shortfilm",
+      title: "Pop films",
+      category: "pop-films",
     },
   ];
+
   return (
     <>
       <Div className="container">
@@ -173,43 +175,55 @@ export default function DocumentairesGallery({ labels, documentaires }) {
               <li className={active === "all" ? "active" : ""}>
                 <span onClick={() => setActive("all")}>All</span>
               </li>
-              {categoryMenu.map((item, index) => (
-                <li className={active === item.category ? "active" : ""} key={index}>
-                  <span onClick={() => setActive(item.category)}>{item.title}</span>
-                </li>
-              ))}
+              {categoryMenu.map((item, index) => {
+                return (
+                  <li className={active === item.category ? "active" : ""} key={index}>
+                    <span
+                      onClick={() => {
+                        console.log("Clicked category:", item.category);
+                        setActive(item.category);
+                      }}>
+                      {item.title}
+                    </span>
+                  </li>
+                );
+              })}
             </ul>
           </Div>
         </Div>
       </Div>
       <Spacing lg="90" md="45" />
       <Div className="cs-masonry_4_col">
-        {documentaires.slice(0, itemShow).map((item, index) => (
-          <Div className={`${active === "all" ? "" : !(active === item.category) ? "d-none" : ""}`} key={index}>
-            <Div className="cs-portfolio cs-style1 cs-type2" style={{ height: `${item.height}px` }}>
-              <Div className="cs-lightbox_item">
-                <ModalImage small={item.src} large={item.srcLg} alt={item.title} />
-              </Div>
-              <Div className="cs-portfolio_hover" />
-              <span className="cs-plus" />
-              <Div className="cs-portfolio_bg cs-bg" style={{ backgroundImage: `url("${item.srcLg}")` }} />
-              <Div className="cs-portfolio_info">
-                <Div className="cs-portfolio_info_bg cs-accent_bg" />
-                <h2 className="cs-portfolio_title">{item.title}</h2>
-                <Div className="cs-portfolio_subtitle">{item.subtitle}</Div>
+        {documentaires.map((item, index) => {
+          const shouldDisplay =
+            (active === "all" || active === item.category) && (active === "all" ? index < visibleCount : true);
+
+          console.log("Item:", item.title, "Category:", item.category);
+          return shouldDisplay ? (
+            <Div className={`${active === "all" ? "" : !(active === item.category) ? "d-none" : ""}`} key={index}>
+              <Div className="cs-portfolio cs-style1 cs-type2" style={{ height: `${item.height}px` }}>
+                <Div className="cs-lightbox_item">
+                  <ModalImage small={item.src} large={item.srcLg} alt={item.title} />
+                </Div>
+                <Div className="cs-portfolio_hover" />
+                <span className="cs-plus" />
+                <Div className="cs-portfolio_bg cs-bg" style={{ backgroundImage: `url("${item.srcLg}")` }} />
+                <Div className="cs-portfolio_info">
+                  <Div className="cs-portfolio_info_bg cs-accent_bg" />
+                  <h2 className="cs-portfolio_title">{item.title}</h2>
+                  <Div className="cs-portfolio_subtitle">{item.subtitle}</Div>
+                </Div>
               </Div>
             </Div>
-          </Div>
-        ))}
+          ) : null;
+        })}
       </Div>
       <Div className="container">
         <Div className="text-center">
-          {documentaires.length <= itemShow ? (
-            ""
-          ) : (
+          {visibleCount < documentaires.length && (
             <>
               <Spacing lg="65" md="40" />
-              <span className="cs-text_btn" onClick={() => setItemShow(itemShow + 4)}>
+              <span className="cs-text_btn" onClick={() => setVisibleCount(visibleCount + 8)}>
                 <span>Load More</span>
                 <Icon icon="bi:arrow-right" />
               </span>
