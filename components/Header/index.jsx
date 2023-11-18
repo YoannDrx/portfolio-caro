a0892c543f186d44c5712ca4ba8b924b2d6b0a65
YoannDrx <yoann.andrieux@gmail.com>
2023-11-18 18:37:44 +0100

    Update footer and header components

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -47,7 +47,7 @@ export default function Header({ variant }) {
             <Div className="cs-main_header_in">
               <Div className="cs-main_header_left">
                 <Link className="cs-site_branding" href="/">
-                  <img src="/images/logo.svg" alt="Logo" />
+                  <img src="/images/logo/logo-cs.png" alt="Logo" />
                 </Link>
               </Div>
               <Div className="cs-main_header_center">
