bfcab8a627de699fb5d88d9962afbf425ea0ff2b
YoannDrx <yoann.andrieux@gmail.com>
2023-11-19 13:55:48 +0100

    Add new image files to portfolio

---
+++ portfolio-data.js
@@ -0,0 +1,38 @@
+const portfolioData = [
+  {
+    title: "Colorful Art Work",
+    subtitle: "See Details",
+    href: "/portfolio/portfolio-details",
+    src: "/images/portfolio_4.jpeg",
+    category: "albums-librairie-musicale",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    releaseDate: "2021-01-01",
+    genre: "Documentaire",
+    compositeurs: [
+      {
+        name: "Jean-Michel Bernard",
+        compoImg: "/images/jean-michel-bernard.jpg",
+        link: "https://www.jeanmichelbernard.com/",
+      },
+    ],
+    synchronisation: [
+      {
+        name: "Jean-Michel Bernard",
+        synchoImg: "/images/jean-michel-bernard.jpg",
+        link: "https://www.jeanmichelbernard.com/",
+      },
+    ],
+    clip: {
+      title: "Clip1",
+      link: "https://www.youtube.com/watch?v=9yj7DQvO9XU",
+    },
+    events: [
+      {
+        eventImg: "/images/jean-michel-bernard.jpg",
+      },
+    ],
+  },
+];
+
+export default portfolioData;
