0c4b8b30959489dd16c5eaf9a9318f5bd1b804bb
YoannDrx <yoann.andrieux@gmail.com>
2023-12-17 21:13:18 +0100

    WIP : Refactor ComposerCard and PortfolioDetails components

--- components/ComposerCard.js
+++ components/ComposerCard.js
@@ -2,6 +2,9 @@ import React from "react";
 import Link from "next/link";
 
 const ComposerCard = ({ composer }) => {
+  // Récupérer tous les liens disponibles
+  const links = Object.entries(composer.links || {});
+
   return (
     <div style={{ display: "flex", alignItems: "center", marginBottom: "20px" }}>
       <img
@@ -9,9 +12,19 @@ const ComposerCard = ({ composer }) => {
         alt={composer.name}
         style={{ width: "50px", height: "50px", borderRadius: "50%", marginRight: "10px" }}
       />
-      <Link href={composer.externalLink} style={{ textDecoration: "none", color: "lightgoldenrodyellow" }}>
-        {composer.name}
-      </Link>
+      <div>
+        <div>{composer.name}</div>
+        {links.length > 0 && <span> (</span>}
+        {links.map(([key, value], index) => (
+          <span key={key}>
+            {index > 0 && ", "}
+            <Link href={value} style={{ textDecoration: "none" }} target="_blank" rel="noopener noreferrer" className="link">
+              {key.charAt(0).toUpperCase() + key.slice(1)}
+            </Link>
+          </span>
+        ))}
+        {links.length > 0 && <span>)</span>}
+      </div>
     </div>
   );
 };
